1e620a4772a34fb251b959b8b5c0c61c37b1f16e
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-30 03:12:06 +0200

    configured api sub-app & prod server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+const api = express();
 
+// variables
 const port = process.env.PORT || 8080;
+const appClientPath = path.join(__dirname, "..", "client", "build");
+
+//set app locals
+app.locals.title = "Dee";
+app.locals.name = "Dee App";
+
+// set middlewares
+express.json();
+app.use(express.static(appClientPath)); // tell express to serve dee app static files
+app.use("/api", api); // set api as a sub app.
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(appClientPath, "index.html"));
+});
+
+api.get("/*", (req, res) => {
+  res.send("<h1>#Hello From Dee Api</h1>");
+});
 
 app.listen(port, () => {
   console.log("Dee Server is Up and Running ...");
-});
-
-app.get("/", (req, res) => {
-  res.send("<h1>#Hello From Dee</h1>");
 });
