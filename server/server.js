18fe8403082782abd1f67a6bfc1a729896ceba57
Osama Najjar <osama.najjar.m@gmail.com>
2018-09-30 02:20:16 +0200

    added and configured nodemon as dev dep

--- server/server.js
+++ server/server.js
@@ -6,3 +6,7 @@ const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log("Dee Server is Up and Running ...");
 });
+
+app.get("/", (req, res) => {
+  res.send("<h1>#Hello From Dee</h1>");
+});
