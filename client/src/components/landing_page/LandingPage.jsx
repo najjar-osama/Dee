ebe5bfb7a97fb5a0df0759ba11e28225ca881b76
Osama Najjar <osama.najjar.m@gmail.com>
2018-10-02 03:13:57 +0200

    created LandingPage component

---
+++ client/src/components/landing_page/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import BackgroundWrapper from "./BackgroundWrapper";
+
+const LandingPage = () => (
+  <div>
+    <BackgroundWrapper>
+      <Header />
+      <Footer />
+    </BackgroundWrapper>
+  </div>
+);
+
+export default LandingPage;
