fc7a226029a37e103f8ee9cdfcca1ee5319d82f3
Osama Najjar <osama.najjar.m@gmail.com>
2018-10-02 03:28:11 +0200

    fixed export bug

--- client/src/scripts/resetEventListener.js
+++ client/src/scripts/resetEventListener.js
@@ -1,6 +1,6 @@
-export default (resetEventListenr = () => {
+export default () => {
   if ("addEventListener" in window) return;
   window.addEventListener = (type, cb) => {
     window.attachEvent(`on${type}`, cb);
   };
-});
+};
