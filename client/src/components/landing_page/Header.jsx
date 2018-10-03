25ee45e6cde38778cd3a2d72d4e5e19fa9b5382c
Osama Najjar <osama.najjar.m@gmail.com>
2018-10-04 01:19:48 +0200

    prevent selecting header texts

--- client/src/components/landing_page/Header.jsx
+++ client/src/components/landing_page/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => (
   <header className="header container">
     <h1 className="header-title">Dee!</h1>
-    <h2 className="header-subtitle">Your new way of communication</h2>
+    <h2 className="header-subtitle">Your new way of communication.</h2>
   </header>
 );
 
