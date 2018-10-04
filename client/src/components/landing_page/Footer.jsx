8396f23d65d090798fcd15971bf1ac0113196ba7
Osama Najjar <osama.najjar.m@gmail.com>
2018-10-04 03:02:54 +0200

    set footer position

--- client/src/components/landing_page/Footer.jsx
+++ client/src/components/landing_page/Footer.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
-const Footer = () => <div>Footer</div>;
+const Footer = () => (
+  <footer className="footer container">
+    <div className="footer-body">
+      <span>{`© Dee! ${new Date(Date.now()).getFullYear()}.`}</span>
+    </div>
+  </footer>
+);
 
 export default Footer;
