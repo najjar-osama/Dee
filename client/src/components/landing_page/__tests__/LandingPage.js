b7e8bbcf3281fbf92aeab53306b31fb66ef68380
Osama Najjar <osama.najjar.m@gmail.com>
2018-10-04 04:07:33 +0200

    full landing page tests

--- client/src/components/landing_page/__tests__/LandingPage.js
+++ client/src/components/landing_page/__tests__/LandingPage.js
@@ -1,19 +1,39 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 import Header from "../Header";
 import Footer from "../Footer";
-import LandingPage from "../LandingPage";
 import BackgroundWrapper from "../BackgroundWrapper";
-import { shallow } from "enzyme";
+import LandingPage from "../LandingPage";
 
-describe("Test Landing Page Components", () => {
+describe("Test Landing Page:", () => {
   it("Renders Header properly & without  crashing", () => {
-    const headerComp = shallow(<Header />);
+    const wrapper = shallow(<Header />);
     const title = <h1 className="header-title">Dee!</h1>;
     const subtitle = (
       <h2 className="header-subtitle">Your new way of communication.</h2>
     );
-    expect(headerComp.contains(title)).toEqual(true);
-    expect(headerComp.contains(subtitle)).toEqual(true);
+    expect(wrapper.contains(title)).toBeTruthy();
+    expect(wrapper.contains(subtitle)).toBeTruthy();
+  });
+
+  it("Renders Footer properly & without  crashing", () => {
+    const wrapper = shallow(<Footer />);
+    const footerText = (
+      <span>{`© Dee! ${new Date(Date.now()).getFullYear()}.`}</span>
+    );
+
+    expect(wrapper.contains(footerText)).toBeTruthy();
+  });
+
+  it("Renders Background Slides properly & without  crashing", () => {
+    const wrapper = shallow(<BackgroundWrapper />);
+    expect(wrapper.find(".bg-wrapper")).toHaveLength(1);
+    expect(wrapper.find(".bg")).toHaveLength(6);
+  });
+
+  test("Landing page should be rendered properly", () => {
+    const compsTree = renderer.create(<LandingPage />).toJSON();
+    expect(compsTree).toMatchSnapshot();
   });
 });
